6d5def47434272888c4fbe04002628d1de56aebc
pew333777 <121802571+pew333777@users.noreply.github.com>
2023-12-11 14:07:39 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -28,13 +28,11 @@ const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
 const nftContract = new web3.eth.Contract(/* 合约ABI */, /* 合约地址 */);
 
 // 监听 Transfer 事件
-nftContract.events.Transfer({})
-.on('data', async (event) => {
-    console.log('Transfer event:', event);
-    nftContract.events.Transfer({
+nftContract.events.Transfer({
     fromBlock: 'latest'
 })
 .on('data', async (event) => {
+    console.log('Transfer event:', event);
     const { from, to, tokenId } = event.returnValues;
 
     // 检查 'to' 地址是否是我们的用户
@@ -43,23 +41,6 @@ nftContract.events.Transfer({})
         // 玩家获得了 NFT，更新数据库
         const newPlayerNFT = new PlayerNFT({
             PlayerID: player._id,
-            NFTContractAddress: nftContract.options.address,
-            TokenID: tokenId
-        });
-        await newPlayerNFT.save();
-    }
-
-    nftContract.events.Transfer({
-    fromBlock: 'latest'
-})
-.on('data', async (event) => {
-    const { from, to, tokenId } = event.returnValues;
-
-    // 处理接收者 (to 地址) 获得了 NFT
-    const receiver = await Player.findOne({ walletAddress: to });
-    if (receiver) {
-        const newPlayerNFT = new PlayerNFT({
-            PlayerID: receiver._id,
             NFTContractAddress: nftContract.options.address,
             TokenID: tokenId
         });
@@ -80,60 +61,15 @@ nftContract.events.Transfer({})
 })
 .on('error', console.error);
 
-})
-.on('error', console.error);
-
-})
-.on('error', (error) => {
-    console.error('Error listening for Transfer events:', error);
-});
-
 // 使用 cron 每 10 秒更新一次数据库
 cron.schedule('*/10 * * * * *', async () => {
     console.log('Updating database...');
-    const PlayerNFT = require('./models/playerNFT'); // 确保引入 PlayerNFT 模型
-
-cron.schedule('*/10 * * * * *', async () => {
-    console.log('Checking for NFT updates...');
-
-    // 假设您有一个函数来获取所有玩家的 NFT
-    const allNFTs = await getAllNFTsFromBlockchain();
-
-    for (const nft of allNFTs) {
-        const { owner, tokenId } = nft;
-
-        // 检查数据库中是否已有此 NFT
-        const existingNFT = await PlayerNFT.findOne({ TokenID: tokenId });
-
-        if (!existingNFT) {
-            // 如果数据库中没有这个 NFT，则添加它
-            const newPlayerNFT = new PlayerNFT({
-                PlayerID: owner, // 假设 owner 是玩家 ID
-                NFTContractAddress: nftContract.options.address,
-                TokenID: tokenId
-            });
-            await newPlayerNFT.save();
-        } else {
-            // 如果数据库中有这个 NFT，可以更新相关信息
-            // ...
-        }
-    }
-
-    console.log('NFT updates checked');
-});
-
-async function getAllNFTsFromBlockchain() {
-    // 实现从区块链获取所有 NFT 的逻辑
-    // 这可能涉及到调用智能合约的方法
-    // 返回格式：[{ owner: '0x...', tokenId: '123' }, ...]
-}
-
-    console.log('Database updated');
+    // 实现定期更新数据库的逻辑...
 });
 
 // 定义更新玩家 NFT 表的 API 接口
 app.post('/api/update_player_nft', async (req, res) => {
-    // API 逻辑
+    // API 逻辑...
 });
 
 // 设置静态文件目录
@@ -157,4 +93,3 @@ app.listen(port, () => {
 });
 
 module.exports = app;
-
